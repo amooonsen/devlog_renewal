d6513d9a58c6c4aa59eb5cc1aa2d8aaf75349af8
CHOGYEONGMOON <chonghocho72@gmail.com>
2026-02-12 16:10:53 +0900

    chore: vercel.json 수정

--- apps/blog/next.config.ts
+++ apps/blog/next.config.ts
@@ -7,6 +7,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const nextConfig: NextConfig = {
   outputFileTracingRoot: path.join(__dirname, "../../"),
   transpilePackages: ["@repo/ui", "@repo/database", "@repo/types"],
+  output: "standalone",
 };
 
 export default nextConfig;
